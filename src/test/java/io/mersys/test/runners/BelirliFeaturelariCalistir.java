39d8699cbb34b2ed627b3dd6845cef4ac31c4095
HaQaN <hakann.tasdelenn@gmail.com>
2022-12-07 21:55:27 +0300

    Örnek runner class tanımlandı

---
+++ src/test/java/io/mersys/test/runners/BelirliFeaturelariCalistir.java
@@ -0,0 +1,25 @@
+package io.mersys.test.runners;
+
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        plugin = {
+                "rerun:target/rerun.txt",
+                "json:target/cucumber.json",
+                "timeline:target/timeline-report"
+        },
+        features = {"src/test/resources/featureFiles/login.feature",
+                "src/test/resources/featureFiles/country.feature"
+        },
+        glue = {"io/mersys/test/stepDefinitions"},
+        dryRun = false,    // true olduğunda testi çalıştırma sadece feature lara
+        // ait steplerin varlığını kontrol eder.
+        // false olduğunda ise testi çalıştırır.
+        tags = "",
+        publish = true
+
+)
+public class BelirliFeaturelariCalistir extends AbstractTestNGCucumberTests {   // abstract class a extend etmezsek çalışmaz
+}
