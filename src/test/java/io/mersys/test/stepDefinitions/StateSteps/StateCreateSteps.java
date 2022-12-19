256dd959d71715074b1def5d64449db2038e2af0
Raziyanur <108152051+raziyanur@users.noreply.github.com>
2022-12-19 12:51:27 +0600

    State Steps tanimlandi

---
+++ src/test/java/io/mersys/test/stepDefinitions/StateSteps/StateCreateSteps.java
@@ -0,0 +1,76 @@
+package io.mersys.test.stepDefinitions.StateSteps;
+
+import com.github.javafaker.Faker;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.mersys.test.pages.DialogContent;
+import io.mersys.test.pages.LeftNav;
+import io.mersys.test.pages.StatesContent;
+import io.mersys.test.utilities.BaseDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Action;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
+
+public class StateCreateSteps {
+    LeftNav ln = new LeftNav();
+    DialogContent dc = new DialogContent();
+    Faker fakeData = new Faker();
+    StatesContent sk= new StatesContent();
+    Actions actions=new Actions(BaseDriver.getDriver());
+    @And("Navigate to State page")
+    public void navigateToStatePage() {
+        ln.findAndClick("setupOne"); // Setup Click
+        ln.findAndClick("parameters"); // Parameters Click
+        ln.findAndClick("states"); // Countries Click
+
+    }
+
+    @When("Create a new State from existing Country")
+    public void createANewStateFromExistingCountry() {
+        sk.findAndClick("addState");
+        actions.moveToElement(sk.selectCountryinWindow).click().sendKeys("Australia").click().build().perform();
+        sk.findAndSend("nameInput","melek sehir");
+        sk.findAndSend("shortName","mk");
+        sk.findAndClick("saveButton");
+
+
+    }
+
+
+    @When("Create a new country from New State window")
+    public void createANewCountryFromNewStateWindow() {
+        sk.findAndClick("addState");
+        sk.findAndClick("addCountry");
+        sk.findAndSend("countryNameInput","Kazakyeli");
+        dc.findAndSend("codeInput", "KY");
+        sk.findAndClick("stateExists");
+        sk.findAndClick("countrySaveButton");
+
+    }
+
+    @And("Create a new State")
+    public void createANewState() {
+        actions.moveToElement(sk.selectCountryinWindow).click().sendKeys("Kazakyeli").click().build().perform();
+        sk.findAndSend("nameInput","Almaty");
+        sk.findAndSend("shortName","AL");
+        sk.findAndClick("saveButton");
+    }
+
+    @When("Create a new country without state from New State window")
+    public void createANewCountryWithoutStateFromNewStateWindow() {
+        sk.findAndClick("addState");
+        sk.findAndClick("addCountry");
+        sk.findAndSend("countryNameInput","Jana Khazak");
+        dc.findAndSend("codeInput", "JK");
+        sk.findAndClick("countrySaveButton");
+    }
+
+    @Then("The created Country not found and new state was not successfully created")
+    public void theCreatedCountryNotFoundAndNewStateWasNotSuccessfullyCreated() {
+        actions.moveToElement(sk.selectCountryinWindow).click().build().perform();
+        for (WebElement e : sk.countryNameList)
+            Assert.assertFalse(e.getText().toLowerCase().contains("Jana Kazak".toLowerCase()));
+    }
+}
