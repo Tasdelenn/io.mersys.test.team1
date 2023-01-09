bc942acbadfa2516d696d7549268253b8b16866b
Muharrem <m.karapazar2@gmail.com>
2023-01-09 09:39:44 +0300

    Muharrem - Add RestAssured Document

---
+++ src/test/java/Campus_RestAssured/Models/Document.java
@@ -0,0 +1,17 @@
+package Campus_RestAssured.Models;
+
+public class Document {
+
+    private String id;
+    private String name;
+    private String description;
+    private String[] attachmentStages;
+    private Boolean active;
+    private Boolean required;
+    private Boolean useCamera;
+    private String[] translateName;
+    private String schoolId;
+
+
+
+}
